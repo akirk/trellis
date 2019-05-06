a8fb197c50d768b3b1773d8975fb4220c5bb0c03
Lukas Besch <connect@lukasbesch.com>
2019-05-06 12:11:50 +0200

    Introduce bin/vault.sh
    Options:
    encrypt
    decrypt

---
+++ bin/vault.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+shopt -s nullglob
+
+show_usage() {
+  echo "Usage: vault <mode>
+
+<mode> can be ("encrypt", "decrypt")
+
+Examples:
+  vault encrypt
+  vault decrypt
+"
+}
+
+[[ $# -lt 1 ]] && { show_usage; exit 127; }
+
+for arg
+do
+  [[ $arg = -h ]] && { show_usage; exit 0; }
+done
+
+MODE="$1"; shift
+
+if [[ $MODE -ne "encrypt" && $MODE -ne "encrypt" ]]; then
+  echo "Error: $MODE is not a valid vault command."
+  show_usage;
+  exit 127;
+fi
+
+FILES="group_vars/all/vault.yml group_vars/development/vault.yml group_vars/staging/vault.yml"; shift
+VAULT_CMD="ansible-vault $MODE $FILES"
+
+$VAULT_CMD
--- encrypt.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ansible-vault encrypt group_vars/all/vault.yml group_vars/development/vault.yml group_vars/staging/vault.yml
