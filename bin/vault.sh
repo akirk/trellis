4ce3fc43620a342c9b8dc7a0b0e2072aed9fac3d
tivus <mackbird@gmail.com>
2019-08-08 16:50:18 +0200

    update vault.sh

--- bin/vault.sh
+++ bin/vault.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-shopt -s nullglob
 
 show_usage() {
-  echo "Usage: vault <command> [<options>...]
+echo "Usage: vault <command> [<options>...]
 Just a proxy of ansible-vault with all files named vault.yml under the group_vars folder.
-
 See 'ansible-vault --help' for more information.
-
-See 'ansible-vault <command> --help' for more information on a specific
-command.
+See 'ansible-vault <command> --help' for more information on a specific command.
+If you have problems with ansible execute this command in your VM ( cd ~/trellis && ./bin/vault.sh ).
 
 Examples:
   vault encrypt
@@ -18,13 +15,23 @@ Examples:
 "
 }
 
+# Number of arguments passed is less than (lt) 1 show usage and exit with "command not found" (127).
 [[ $# -lt 1 ]] && { show_usage; exit 127; }
 
+# If one of the arguments is -h show the usage and exit with "success" (0).
 for arg
 do
   [[ $arg = -h ]] && { show_usage; exit 0; }
 done
 
-VAULT_CMD="find group_vars/ -name vault.yml -exec ansible-vault $1 {} +"
+dir="group_vars/"
+# If the dirctory does not exits you are probably in the /bin directory.
+[[ ! -d "$dir" ]] && {
+  echo "You are executing this script from the wrong directory.
+Try: './bin/vault.sh $1' in the trellis directory."
+  exit 127;
+}
+
+VAULT_CMD="find $dir -name vault.yml -exec ansible-vault $1 {} +"
 
 $VAULT_CMD
